e5ea2ce2b8b749b36b7d9de62a4a2dd0810fbfb4
Musthafa Faishal <musthafafaishal@gmail.com>
2021-11-08 14:58:13 +0700

    Updates

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,19 @@
-self.addEventListener("install", (event) => {
-  console.log("Installing Service Worker ...");
+import CacheHelper from "./utils/cache-helper";
+import "regenerator-runtime";
 
-  // TODO: Caching App Shell Resource
+const { assets } = global.serviceWorkerOption;
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, "./"]));
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Activating Service Worker ...");
-
-  // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
-  // TODO: Add/get fetch request to/from caches
+  if (!event.request.url.startsWith("http")) {
+    return;
+  }
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
